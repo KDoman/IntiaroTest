e56805300200de1e5014af70cdbd14a8e4d28761
Kacper Domański <116498271+KDoman@users.noreply.github.com>
2024-08-13 12:35:50 +0200

    '21'

--- back-end/index.js
+++ back-end/index.js
@@ -1,41 +1,20 @@
+// index.js
+const express = require("express");
+const cors = require("cors");
+const bodyParser = require("body-parser");
 const { chromium } = require("playwright");
 
-module.exports = async (req, res) => {
-  // Ustawienie nagłówków CORS
-  res.setHeader("Access-Control-Allow-Origin", "*"); // Można zastąpić '*' konkretną domeną, np. 'https://intiaro-front-end.vercel.app'
-  res.setHeader("Access-Control-Allow-Methods", "POST");
-  res.setHeader("Access-Control-Allow-Headers", "Content-Type");
-
-  if (req.method === "OPTIONS") {
-    // Obsługuje preflight request
-    res.status(200).end();
-    return;
-  }
-
-  if (req.method === "POST") {
-    const { transforms, username, password } = req.body;
-    try {
-      await runPlaywrightScript(transforms, username, password);
-      res.status(200).send("Transforms submitted successfully.");
-    } catch (error) {
-      console.error(error);
-      res.status(500).send("Error submitting transforms.");
-    }
-  } else {
-    res.setHeader("Allow", ["POST"]);
-    res.status(405).end(`Method ${req.method} Not Allowed`);
-  }
-};
-
 async function runPlaywrightScript(transforms, username, password) {
-  const browser = await chromium.launch({ headless: true });
+  const browser = await chromium.launch({ headless: false });
   const page = await browser.newPage();
 
+  // Logowanie
   await page.goto("https://intiaro.agitest.pl/admin/products/matrix/add");
   await page.fill("input#id_username", username);
   await page.fill("input#id_password", password);
   await page.click("input[type=submit]");
 
+  // Przesyłanie transformacji
   for (let transform of Object.keys(transforms)) {
     for (let params of Object.keys(transforms[transform])) {
       let formFields = [];
@@ -58,3 +37,22 @@ async function runPlaywrightScript(transforms, username, password) {
 
   await browser.close();
 }
+
+const app = express();
+app.use(cors());
+app.use(bodyParser.json());
+
+app.post("/", async (req, res) => {
+  const { transforms, username, password } = req.body;
+  try {
+    await runPlaywrightScript(transforms, username, password);
+    res.status(200).send("Transforms submitted successfully.");
+  } catch (error) {
+    console.error(error);
+    res.status(500).send("Error submitting transforms.");
+  }
+});
+
+app.listen(3000, () => {
+  console.log(`Server is running on http://localhost:3000`);
+});
