1ed1c95bdb777838c8bbe5a699c077a88ce932fa
Kacper Domański <116498271+KDoman@users.noreply.github.com>
2024-08-13 10:49:32 +0200

    back-end-test

--- back-end/index.js
+++ back-end/index.js
@@ -1,7 +1,41 @@
 const express = require("express");
 const cors = require("cors");
 const bodyParser = require("body-parser");
-const { runPlaywrightScript } = require("./playwright-script");
+const { chromium } = require("playwright");
+
+async function runPlaywrightScript(transforms, username, password) {
+  const browser = await chromium.launch({ headless: false });
+  const page = await browser.newPage();
+
+  // Logowanie
+  await page.goto("https://intiaro.agitest.pl/admin/products/matrix/add");
+  await page.fill("input#id_username", username);
+  await page.fill("input#id_password", password);
+  await page.click("input[type=submit]");
+
+  // Przesyłanie transformacji
+  for (let transform of Object.keys(transforms)) {
+    for (let params of Object.keys(transforms[transform])) {
+      let formFields = [];
+      if (params === "Position") {
+        formFields = ["position_x", "position_y", "position_z"];
+      } else if (params === "Rotation") {
+        formFields = ["rotation_x", "rotation_y", "rotation_z"];
+      } else if (params === "Scale") {
+        formFields = ["scale_x", "scale_y", "scale_z"];
+      }
+
+      for (let i = 0; i < formFields.length; i++) {
+        const field = formFields[i];
+        const value = transforms[transform][params][i];
+        await page.fill(`input[name='${field}']`, value.toString());
+      }
+    }
+    await page.click("input[type=submit][name=_addanother]");
+  }
+
+  await browser.close();
+}
 
 const app = express();
 
@@ -9,11 +43,7 @@ app.use(cors());
 
 app.use(bodyParser.json());
 
-app.get("/", (res, req) => {
-  req.send("Intiaro back-end");
-});
-
-app.post("/submit-transforms", async (req, res) => {
+app.post("/", async (req, res) => {
   const { transforms, username, password } = req.body;
   try {
     await runPlaywrightScript(transforms, username, password);
