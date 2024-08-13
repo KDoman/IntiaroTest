3c6632e63f4be5c7d7559277b66d1406030d2699
Kacper Domański <116498271+KDoman@users.noreply.github.com>
2024-08-13 12:03:54 +0200

    cors install2

--- back-end/api/index.js
+++ back-end/api/index.js
@@ -4,11 +4,7 @@ const express = require("express");
 
 const app = express();
 
-app.use(
-  cors({
-    origin: "*",
-  })
-);
+app.use(cors());
 app.use(express.json());
 
 app.post("/", async (req, res) => {
