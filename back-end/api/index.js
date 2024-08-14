04ce19fcdffd9362aab587ca2e8349bb65cbf120
Kacper Domański <116498271+KDoman@users.noreply.github.com>
2024-08-14 13:25:22 +0200

    'test9'

--- back-end/api/index.js
+++ back-end/api/index.js
@@ -1,56 +1,19 @@
 const express = require("express");
+const cors = require("cors");
+// const bodyParser = require("body-parser");
+const { runPlaywrightScript } = require("./playwright-script");
+
 const app = express();
 
 app.get("/", (req, res) => res.send("Express on Vercel"));
 
 module.exports = app;
 
-// // index.js
-// const express = require("express");
-// const cors = require("cors");
-// const bodyParser = require("body-parser");
-// const { chromium } = require("playwright");
-
-// async function runPlaywrightScript(transforms, username, password) {
-//   const browser = await chromium.launch({ headless: false });
-//   const page = await browser.newPage();
-
-//   // Logowanie
-//   await page.goto("https://intiaro.agitest.pl/admin/products/matrix/add");
-//   await page.fill("input#id_username", username);
-//   await page.fill("input#id_password", password);
-//   await page.click("input[type=submit]");
-
-//   // Przesyłanie transformacji
-//   for (let transform of Object.keys(transforms)) {
-//     for (let params of Object.keys(transforms[transform])) {
-//       let formFields = [];
-//       if (params === "Position") {
-//         formFields = ["position_x", "position_y", "position_z"];
-//       } else if (params === "Rotation") {
-//         formFields = ["rotation_x", "rotation_y", "rotation_z"];
-//       } else if (params === "Scale") {
-//         formFields = ["scale_x", "scale_y", "scale_z"];
-//       }
-
-//       for (let i = 0; i < formFields.length; i++) {
-//         const field = formFields[i];
-//         const value = transforms[transform][params][i];
-//         await page.fill(`input[name='${field}']`, value.toString());
-//       }
-//     }
-//     await page.click("input[type=submit][name=_addanother]");
-//   }
-
-//   await browser.close();
-// }
-
-// const app = express();
-// app.use(
-//   cors({
-//     origin: "*",
-//   })
-// );
+app.use(
+  cors({
+    origin: "*",
+  })
+);
 // app.use(bodyParser.json());
 
 // app.post("/transforms", async (req, res) => {
@@ -64,3 +27,7 @@ module.exports = app;
 //     res.status(500).send("Error submitting transforms.");
 //   }
 // });
+
+app.post("/transforms", (req, res) => {
+  res.send("transforms function");
+});
