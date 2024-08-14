a2a59ee159a75178de58c59c518c765ec0e4214f
Kacper Domański <116498271+KDoman@users.noreply.github.com>
2024-08-14 13:11:59 +0200

    't5'

--- front-end/src/views/Python.jsx
+++ front-end/src/views/Python.jsx
@@ -66,17 +66,20 @@ export const Python = () => {
 
   const handleSubmit = async () => {
     try {
-      const response = await fetch("http://localhost:3000", {
-        method: "POST",
-        headers: {
-          "Content-Type": "application/json",
-        },
-        body: JSON.stringify({
-          transforms,
-          username,
-          password,
-        }),
-      });
+      const response = await fetch(
+        "https://intiaro-back-end.vercel.app/transforms",
+        {
+          method: "POST",
+          headers: {
+            "Content-Type": "application/json",
+          },
+          body: JSON.stringify({
+            transforms,
+            username,
+            password,
+          }),
+        }
+      );
 
       if (response.ok) {
         alert("Transforms submitted successfully.");
